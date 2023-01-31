d98b04cd8838e501b5ee2b3e75d16914778258d5
juansensio <sensio.juan@gmail.com>
2023-01-31 09:14:52 +0100

    update

--- scripts/imagenet.py
+++ scripts/imagenet.py
@@ -1,11 +1,49 @@
 import argparse
 import convnets.train.imagenet.configs as configs
 import convnets.models as models
+from torch.utils.data import DataLoader
+from convnets.train import fit
+import torch
 
 def train(config):
 	print(config)
 	model = getattr(models, config['model'])(config)
-	print(model)
+	# dataset = {
+	# 	'train': Imagenet(trans=config['trans']),
+	# 	'val': Imagenet(trans=config['trans'])
+	# }
+	# dataloader = {
+	# 	'train': DataLoader(
+	# 		dataset['train'], 
+	# 		batch_size=config['batch_size'], 
+	# 		shuffle=True, 
+	# 		num_workers=config['num_workers'],
+	# 		pin_memory=config['pin_memory']
+	# 	),
+	# 	'val': DataLoader(
+	# 		dataset['val'], 
+	# 		batch_size=config['batch_size'], 
+	# 		num_workers=config['num_workers'],
+	# 		pin_memory=config['pin_memory']
+	# 	),
+	# }
+	# optimizer = getattr(torch.optim, config['optimizer'])(**config['optimizer_params'])
+	# criterion = torch.nn.CrossEntropyLoss()
+	# hist = fit(
+	# 	model, 
+	# 	dataloader, 
+	# 	optimizer, 
+	# 	criterion, 
+	# 	device="cpu", 
+	# 	epochs=config['epochs'],
+	# 	overfit=0,
+	# 	log=True,
+	# 	compile=False,
+	# 	on_epoch_end=None,
+	# 	limit_train_batches=0,
+	# 	use_amp = True, 
+	# )
+	# return hist
 	
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Process imagenet.')
