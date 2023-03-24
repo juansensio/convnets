c0fb7d58e8b82e76c55d34e0f1576e632195f27f
juansensio <sensio.juan@gmail.com>
2023-03-24 19:33:00 +0100

    update

--- convnets/train/fit.py
+++ convnets/train/fit.py
@@ -24,7 +24,6 @@ def fit(
 ):
     fabric = L.Fabric(accelerator="auto", devices=1) if fabric is None else fabric
     fabric.launch()
-    print("Training model on", fabric.device)
     # count parameters
     if fabric.global_rank == 0:
         n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -56,53 +55,54 @@ def fit(
             X, y = batch
             optimizer.zero_grad()
             y_hat = model(X)
-            # loss = criterion(y_hat, y)
-            # fabric.backward(loss)
-            # # loss.backward()
-            # optimizer.step()
-            # train_logs['loss'].append(loss.item())
-            # for metric in metrics.keys():
-            #     train_logs[metric].append(metrics[metric](y_hat, y).item())
-            # log = f"Epoch {epoch}/{max_epochs} loss {np.mean(train_logs['loss']):.5f}"
-            # for metric in metrics.keys():
-            #     log += f" {metric} {np.mean(train_logs[metric]):.5f}"
-            # if fabric.global_rank == 0: pbar.set_description(log)
-            # if overfit_batches and batch_ix > overfit_batches:
-            #     break
-            # if limit_train_batches and batch_ix > limit_train_batches:
-            #     break
+            loss = criterion(y_hat, y)
+            fabric.backward(loss)
+            # loss.backward()
+            optimizer.step()
+            train_logs['loss'].append(loss.item())
+            for metric in metrics.keys():
+                train_logs[metric].append(metrics[metric](y_hat, y).item())
+            log = f"Epoch {epoch}/{max_epochs} loss {np.mean(train_logs['loss']):.5f}"
+            for metric in metrics.keys():
+                log += f" {metric} {np.mean(train_logs[metric]):.5f}"
+            if fabric.global_rank == 0: pbar.set_description(log)
+            if overfit_batches and batch_ix > overfit_batches:
+                break
+            if limit_train_batches and batch_ix > limit_train_batches:
+                break
         hist['loss'].append(np.mean(train_logs['loss']))
         for metric in metrics.keys():
             hist[metric].append(np.mean(train_logs[metric]))
-        # if not overfit_batches and 'val' in dataloader and dataloader['val'] is not None:
-        #     val_logs = {'loss': []}
-        #     for metric in metrics.keys():
-        #         val_logs[metric] = []
-        #     model.eval()
-        #     with torch.no_grad():
-        #         pbar = tqdm(dataloader['val'])
-        #         for batch_ix, batch in enumerate(pbar):
-        #             X, y = batch
-        #             y_hat = model(X)
-        #             loss = criterion(y_hat, y)
-        #             val_logs['loss'] .append(loss.item())
-        #             for metric in metrics.keys():
-        #                 val_logs[metric].append(metrics[metric](y_hat, y).item())
-        #             if fabric.global_rank == 0:
-        #                 _log = f"val_loss {np.mean(val_logs['loss']):.5f}"
-        #                 for metric in metrics.keys():
-        #                     _log += f" val_{metric} {np.mean(val_logs[metric]):.5f}"
-        #                 pbar.set_description(_log)
-        #             if limit_val_batches and batch_ix > limit_val_batches:
-        #                 break
-        #     hist['val_loss'].append(np.mean(val_logs['loss']))
-        #     for metric in metrics.keys():
-        #         hist['val_' + metric].append(np.mean(val_logs[metric]))
-        #     if fabric.global_rank == 0:
-        #         log += f" val_loss {np.mean(val_logs['loss']):.5f}"
-        #         for metric in metrics.keys():
-        #             log += f" val_{metric} {np.mean(val_logs[metric]):.5f}"
+        if not overfit_batches and 'val' in dataloader and dataloader['val'] is not None:
+            val_logs = {'loss': []}
+            for metric in metrics.keys():
+                val_logs[metric] = []
+            model.eval()
+            with torch.no_grad():
+                pbar = tqdm(dataloader['val'])
+                for batch_ix, batch in enumerate(pbar):
+                    X, y = batch
+                    y_hat = model(X)
+                    loss = criterion(y_hat, y)
+                    val_logs['loss'] .append(loss.item())
+                    for metric in metrics.keys():
+                        val_logs[metric].append(metrics[metric](y_hat, y).item())
+                    if fabric.global_rank == 0:
+                        _log = f"val_loss {np.mean(val_logs['loss']):.5f}"
+                        for metric in metrics.keys():
+                            _log += f" val_{metric} {np.mean(val_logs[metric]):.5f}"
+                        pbar.set_description(_log)
+                    if limit_val_batches and batch_ix > limit_val_batches:
+                        break
+            hist['val_loss'].append(np.mean(val_logs['loss']))
+            for metric in metrics.keys():
+                hist['val_' + metric].append(np.mean(val_logs[metric]))
+            if fabric.global_rank == 0:
+                log += f" val_loss {np.mean(val_logs['loss']):.5f}"
+                for metric in metrics.keys():
+                    log += f" val_{metric} {np.mean(val_logs[metric]):.5f}"
             # after_val(val_logs)
         hist['lr'].append(optimizer.param_groups[0]['lr'])
+        # fabric.log_dict({k: v[-1] for k, v in hist.items()})
         # on_epoch_end(hist, model, optimizer)
     return hist
